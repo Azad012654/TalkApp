340c20a8f4c10255a8469c83ce3c014566a0b928
Azad012654 <azadchauhan012654@gmail.com>
2023-06-29 23:09:52 +0530

    first commit

---
+++ src/main/java/com/goldenratio/chuglikaro/ChuglikaroApplication.java
@@ -0,0 +1,13 @@
+package com.goldenratio.chuglikaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuglikaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuglikaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldenratio/chuglikaro/ChuglikaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenratio.chuglikaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuglikaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
